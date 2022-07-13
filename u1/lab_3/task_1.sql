38095890c4277b91e3966f05f2e9499c52fce837
annslizh <slizh20042004@gmail.com>
2022-07-13 11:51:21 +0300

    Changed task1

--- u1/lab_3/task_1.sql
+++ u1/lab_3/task_1.sql
@@ -2,14 +2,14 @@
 create table t
   ( a int,
     b varchar2(4000) default rpad('*',4000,'*'),
-    c varchar2(3000) default rpad('*',3000,'*')
+    c varchar2(4000) default rpad('*',4000,'*')
    );
    
 -- Inserting data. Deleting row where a = 2 and insert one more row.
 insert into t (a) values (1);
 insert into t (a) values (2);
 insert into t (a) values (3);
-commit;
+commit; 
 delete from t where a = 2 ;
 commit;
 insert into t (a) values (4);
