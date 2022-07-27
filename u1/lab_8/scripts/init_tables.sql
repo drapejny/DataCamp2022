12690c90b0411b1c0c8e119faf6590f341ae6664
Anton Slizh <antonslizh@gmail.com>
2022-07-27 14:12:40 +0300

    Added partitioning to fact table

--- u1/lab_8/scripts/init_tables.sql
+++ u1/lab_8/scripts/init_tables.sql
@@ -97,32 +97,39 @@ create table DIM_STORE (
 /*==============================================================*/
 /* Table: FCT_SALE                                              */
 /*==============================================================*/
-create table FCT_SALE (
-   "date_id"            DATE,
-   "product_id"         NUMBER,
-   "customer_id"        NUMBER,
-   "store_id"           NUMBER,
-   "geo_id"             NUMBER,
-   "amount"             NUMBER,
-   "pos_transaction"    VARCHAR2(20 CHAR)
-) tablespace ts_sa_fct_sales_01;
+CREATE TABLE FCT_SALE (
+   date_id            DATE,
+   product_id         NUMBER,
+   customer_id        NUMBER,
+   store_id           NUMBER,
+   geo_id             NUMBER,
+   amount             NUMBER,
+   pos_transaction    VARCHAR2(20 CHAR)
+)
+PARTITION BY RANGE (date_id) (
+    PARTITION SALES_2019 VALUES LESS THAN (to_date('01/01/2020','DD/MM/YYYY')) TABLESPACE ts_sa_fct_sales_01,
+    PARTITION SALES_2020 VALUES LESS THAN (to_date('01/01/2021','DD/MM/YYYY')) TABLESPACE ts_sa_fct_sales_01,
+    PARTITION SALES_2021 VALUES LESS THAN (to_date('01/01/2022','DD/MM/YYYY')) TABLESPACE ts_sa_fct_sales_01,
+    PARTITION SALES_2022 VALUES LESS THAN (to_date('01/01/2023','DD/MM/YYYY')) TABLESPACE ts_sa_fct_sales_01
+)
+TABLESPACE ts_sa_fct_sales_01;
 
 alter table FCT_SALE
-   add constraint FK_FCT_SALE_REFERENCE_DIM_GEN_ foreign key ("date_id")
-      references DIM_GEN_DATE ("date_id");
+   add constraint FK_DIM_GEN_DATE foreign key (date_id)
+      references DIM_GEN_DATE (date_id);
 
 alter table FCT_SALE
-   add constraint FK_FCT_SALE_REFERENCE_DIM_PROD foreign key ("product_id")
-      references DIM_PRODUCT ("product_id");
+   add constraint FK_DIM_PRODUCT foreign key (product_id)
+      references DIM_PRODUCT (product_id);
 
 alter table FCT_SALE
-   add constraint FK_FCT_SALE_REFERENCE_DIM_CUST foreign key ("customer_id")
-      references DIM_CUSTOMER ("customer_id");
+   add constraint FK_DIM_CUSTOMER foreign key (customer_id)
+      references DIM_CUSTOMER (customer_id);
 
 alter table FCT_SALE
-   add constraint FK_FCT_SALE_REFERENCE_DIM_STOR foreign key ("store_id")
-      references DIM_STORE ("store_id");
+   add constraint FK_DIM_STORE foreign key (store_id)
+      references DIM_STORE (store_id);
 
 alter table FCT_SALE
-   add constraint FK_FCT_SALE_REFERENCE_DIM_GEO_ foreign key ("geo_id")
-      references DIM_GEO_LOCATION ("geo_id");
+   add constraint FK_DIM_GEO_LOCATION foreign key (geo_id)
+      references DIM_GEO_LOCATION (geo_id);
